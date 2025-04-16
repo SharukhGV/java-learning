e16568a9e0c95b5c70b88cda1f2be9ca919aa969
Sharukh GV <sharukh.g.velupillai@gmail.com>
2025-04-16 19:23:50 -0400

    learned variable initialization and declaration and new line breaks

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,12 @@ public class HelloWorld{
 
     public static void main(String[] args){
         System.out.println("Hello World! I, Sharukh, am learning Java");
+        System.out.println("Hello Sharukh!");
     }
+
+    // public static void helloSharukh(String[] args){
+    //     System.out.println("Hello Sharukh!");
+    // }
+
+
 }
---
+++ VariableFun.java
@@ -0,0 +1,38 @@
+
+
+public class VariableFun{
+
+
+    public static void main(String[] args){
+        // Variable intialization and declaration
+        int age = 30;
+        // variable initialization
+        int centuryInYears;
+        // variable declaration
+        centuryInYears = 100;
+        // variable initialization and declaration
+        String name = "Sharukh";
+        String homeTown="Queens, NY";
+// symbolic constants example below:
+        final int SOME_NUM = 150;
+
+    System.out.println(name +" is " + age + ".");
+    // println makes an extra line after line is printed
+    System.out.print(centuryInYears + " years makes one century.");
+    // print only prints line with no additional lines
+System.out.print("This line of code printed will have a space after it because we will use the back-slash 'n' \n");
+    // print only prints line with no additional lines
+    System.out.print("This code line is on a new line thanks to previous code line of back-slash 'n'. This is called escape sequence in Java");
+    System.out.print("This code line is on a new line thanks to previous code line of back-slash 'n'. This is called escape sequence in Java");
+    System.out.print("\n");
+
+    System.out.print(name + " is from " + homeTown);
+
+
+
+    }
+
+
+
+
+}
