0fc658f26ecad803f0b5df4acd49a43f50b2d5ab
Sharukh GV <sharukh.g.velupillai@gmail.com>
2025-04-17 14:21:47 -0400

    learning conversion type

---
+++ ConversionFun.java
@@ -0,0 +1,14 @@
+public class ConversionFun {
+
+public static void main(String[] args){
+
+    double myDouble = 3.14;
+    float myFloat = (float)3.14; //narrowing or lossy conversion, can also be written 3.14f
+    double yourDouble = myFloat; //widening/lossless conversion
+
+}
+
+
+// byte, float, long, char, boolean, double, short, int
+
+}
